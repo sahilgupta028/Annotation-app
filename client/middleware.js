0347507b705188a7f1625be146cb742c67373fba
sahilgupta028 <149893310+sahilgupta028@users.noreply.github.com>
2026-01-31 00:01:18 +0530

    done

--- client/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  const token = req.cookies.get("token") || null;
-
-  if (req.nextUrl.pathname.startsWith("/canvas") && !token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-}
--- client/src/app/canvas/page.js
+++ client/src/app/canvas/page.js
@@ -1,12 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import CanvasStage from "../../components/CanvasStage";
 
 export default function CanvasPage() {
-  const router = useRouter();
 
   return (
     <>
