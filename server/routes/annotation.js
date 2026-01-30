9d1394c703c2219ce5522332877f2eaf8fc20edb
sahilgupta028 <149893310+sahilgupta028@users.noreply.github.com>
2026-01-30 22:46:19 +0530

    done

--- server/routes/annotation.js
+++ server/routes/annotation.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Annotation from "../models/Annotations.js";
-import authMiddleware from "../middleware/authmiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
